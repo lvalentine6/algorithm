9796622b9413f0c93aa91ce962900a19986cf1e9
lvalentine6 <ysr1227@naver.com>
2021-09-01 01:34:57 +0900

    체육복 최종 수정

--- doit-algorithm/src/programmers/Gym_clothes2.java
+++ doit-algorithm/src/programmers/Gym_clothes2.java
@@ -6,22 +6,26 @@ import java.util.HashSet;
 public class Gym_clothes2 {
 	public static void main(String[] args) {
 		
-		// 
-		int answer = 5;
-		int[] lost = {4,2,5};
-		int[] reserve = {2,1,4};
+	// 반환값을 n으로 초기화
+	int answer = 5;
+	int[] lost = {4,2,5};
+	int[] reserve = {2,1,4};
 	
+	// 정렬
 	Arrays.sort(lost);
     Arrays.sort(reserve);	
-		
+	
+    // reserve와 lost 배열 초기화
 	HashSet<Integer> rList = new HashSet<>();
 	HashSet<Integer> lList = new HashSet<>();
 	
+	// reserve 배열 등록
 	for (int i : reserve) {
 		rList.add(i);
 	}
 	System.out.println(rList);
 	
+	// lost 배열 등록하면서 체육복 교환
 	for (int i : lost) { 
 		if(rList.contains(i)) {
 			rList.remove(i);
@@ -32,6 +36,7 @@ public class Gym_clothes2 {
 	System.out.println(rList);
 	System.out.println(lList);
 	
+	// 최대값 반환
 	for (int i : lost) { 
 			if(lList.contains(i)) { 
 				if(rList.contains(i-1)) 
