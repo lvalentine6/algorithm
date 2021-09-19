d8e311722fbd1b22ee03fa638fe4f8cde83d1a65
lvalentine6 <ysr1227@naver.com>
2021-09-19 23:58:34 +0900

    소수 만들기

--- doit-algorithm/src/programmers/Prime_number.java
+++ doit-algorithm/src/programmers/Prime_number.java
@@ -2,6 +2,27 @@ package programmers;
 
 public class Prime_number {
 	public static void main(String[] args) {
+		int[] nums = {1,2,7,6,4};
+		int answer = 0;
+		int temp = 0;
 		
+		for(int i=0; i<nums.length; i++) {
+			for(int k=i+1; k<nums.length; k++) {
+				for(int j=i+1; k<nums.length; j++) {
+					temp = nums[i] + nums[k] + nums[j];
+					if(isPrime(temp) == true) {
+						answer++;
+						}
+					}
+			}
+		}
+		System.out.println(answer);
 	}
+		public static boolean isPrime(int temp){
+		        for(int i=2; i<temp; i++){
+		            if(temp % i == 0) return false;
+		        }
+		        return true;
+		    }
+		
 }
