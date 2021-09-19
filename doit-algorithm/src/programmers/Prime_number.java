46fda0bf844f5a697c7dae39e511e2f909881b0d
lvalentine6 <ysr1227@naver.com>
2021-09-20 00:09:25 +0900

    소수 만들기

--- doit-algorithm/src/programmers/Prime_number.java
+++ doit-algorithm/src/programmers/Prime_number.java
@@ -4,12 +4,12 @@ public class Prime_number {
 	public static void main(String[] args) {
 		int[] nums = {1,2,7,6,4};
 		int answer = 0;
-		int temp = 0;
 		
+		// 3자리수 뽑기
 		for(int i=0; i<nums.length; i++) {
 			for(int k=i+1; k<nums.length; k++) {
-				for(int j=i+1; k<nums.length; j++) {
-					temp = nums[i] + nums[k] + nums[j];
+				for(int j=k+1; j<nums.length; j++) {
+					int temp = nums[i] + nums[k] + nums[j];
 					if(isPrime(temp) == true) {
 						answer++;
 						}
@@ -18,6 +18,8 @@ public class Prime_number {
 		}
 		System.out.println(answer);
 	}
+	
+	// 소수 판별 메소드
 		public static boolean isPrime(int temp){
 		        for(int i=2; i<temp; i++){
 		            if(temp % i == 0) return false;
