dce758902cdc9584c4dc23fbb6f9fdb54d3dbfe9
lvalentine6 <ysr1227@naver.com>
2021-09-21 02:36:36 +0900

    부족한 금액 채우기

--- doit-algorithm/src/programmers/Price.java
+++ doit-algorithm/src/programmers/Price.java
@@ -2,6 +2,25 @@ package programmers;
 
 public class Price {
 	public static void main(String[] args) {
+		int price = 3;
+		int money = 20;
+		int count = 4;
+		long temp = 0;
+		long answer = 0;
+		
+		for(int i=1; i<count+1; i++) {
+			temp += i * price; 
+		}
+		
+		answer = temp - money;
+		
+		if(answer > 0) {
+			System.out.println(answer);
+		}
+		else {
+			answer = 0;
+			System.out.println(answer);
+		}
 		
 	}
 }
